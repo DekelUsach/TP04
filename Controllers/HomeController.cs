7737829d0494a0942619096255ab095a769578cd
DEKEL USACH <48386383@ort>
2024-06-06 16:05:44 -0300

    commit 10

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,11 +28,12 @@ public class HomeController : Controller
     }
     public IActionResult GuardarPaquete (int Destino, int Hotel, int Aereo, int Excursion)
     {
+        
+
         if(Destino >= 1 && Destino<= 10 && Excursion >= 1 && Excursion<= 10 && Hotel >= 1 && Hotel<= 10 && Aereo >= 1 && Aereo<= 10){
-            Paquete nuevoPaquete = new Paquete(Hotel.ToString(), Excursion.ToString(), Aereo.ToString());
+            Paquete nuevoPaquete = new Paquete("Hotel "+Hotel.ToString()+".webp","Excu " + Excursion.ToString()+ ".webp", "Avion "+ Aereo.ToString()+ ".webp");
             ORTWorld.IngresarPaquete(Destino.ToString(), nuevoPaquete);
              return RedirectToAction( "Index" );
-        
         }
         else
         {
@@ -41,8 +42,10 @@ public class HomeController : Controller
             ViewBag.ListaAviones = ORTWorld.ListaAereos;
             ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
             ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
+
             return View("SelectPaquete");
         }
+        
    
     }
 }
