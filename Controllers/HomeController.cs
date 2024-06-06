9fd2264563103b00204715a28a01a7614e37b20b
DEKEL USACH <48386383@ort>
2024-06-06 15:03:16 -0300

    commit 9

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,29 +15,34 @@ public class HomeController : Controller
 
      public IActionResult Index()
     {
-        /*Paquete manolo = new Paquete();
-        ORTWorld jfeoifje = new ORTWorld();
-        jfeoifje.IngresarPaquete("destino",manolo);
-        ViewBag.uoewieu = jfeoifje.Paquetes();*/
+        ViewBag.Dic = ORTWorld.Paquetes;
         return View();
     }
     public IActionResult SelectPaquete()
     {
-        @ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
-        @ViewBag.ListaAviones = ORTWorld.ListaAereos;
-        @ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
-        @ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
+        ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
+        ViewBag.ListaAviones = ORTWorld.ListaAereos;
+        ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
+        ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
         return View();
     }
     public IActionResult GuardarPaquete (int Destino, int Hotel, int Aereo, int Excursion)
     {
         if(Destino >= 1 && Destino<= 10 && Excursion >= 1 && Excursion<= 10 && Hotel >= 1 && Hotel<= 10 && Aereo >= 1 && Aereo<= 10){
+            Paquete nuevoPaquete = new Paquete(Hotel.ToString(), Excursion.ToString(), Aereo.ToString());
+            ORTWorld.IngresarPaquete(Destino.ToString(), nuevoPaquete);
+             return RedirectToAction( "Index" );
+        
+        }
+        else
+        {
             ViewBag.MensajeError = "Error, debe ingresar un numero valido";
+             ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
+            ViewBag.ListaAviones = ORTWorld.ListaAereos;
+            ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
+            ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
             return View("SelectPaquete");
         }
-        Paquete nuevoPaquete = new Paquete(Hotel.ToString(), Excursion.ToString(), Aereo.ToString());
-        ORTWorld.IngresarPaquete(Destino.ToString(), nuevoPaquete);
-        ViewBag.Dic = ORTWorld.Paquetes;
-        return ViewBag("Index");
+   
     }
 }
